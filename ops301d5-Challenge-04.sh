1c5fa4c72dcb675746fdb0d1096d03c87fa37bd2
Jon Salhus <salhusj96@gmail.com>
2022-09-01 19:57:36 -0400

    adds Ops Challenge 04

---
+++ ops301d5-Challenge-04.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Script: Ops 301d5 Ops Challenge Solution 04
+# Author: Jon Salhus                  
+# Date of latest revision: 9/1/2022   
+# Purpose: Launches a system menu with four options utilizing a loop and conditionals
+
+PS3='Select option: '
+
+select option in Hello Ping IPinfo Quit
+    do
+        if [[ $option = 'Hello' ]]
+        then
+            echo "Hello world!"
+        elif [[ $option = 'Ping' ]]
+        then   
+            ping -c 4 127.0.0.1
+        elif [[ $option = 'IPinfo' ]]
+        then
+            ip addr show
+        else [[ $option = 'Quit' ]]
+            break
+        fi
+    done
