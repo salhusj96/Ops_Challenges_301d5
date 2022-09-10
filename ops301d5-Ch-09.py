fbf11328d487fb803930701ac4fdf35c61998d5d
Jon Salhus <salhusj96@gmail.com>
2022-09-09 20:13:57 -0400

    adds Ops Ch 09

---
+++ ops301d5-Ch-09.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+# Script: Ops 301d5 Ops Challenge Solution 09
+# Author: Jon Salhus                  
+# Date of latest revision: 9/9/2022   
+# Purpose: Prints specific elements from a list
+
+# Main
+
+grocerylist = ['Beef', 'Chicken', 'Fish', 'Broccoli', 'Potatoes', 'Celery', 'Spinach', 'Bacon', 'Cheese', 'Onions']
+
+print(grocerylist)
+print(grocerylist[3])
+print(grocerylist[5:11])
+grocerylist[6] = 'Garlic'
+print(grocerylist)
+
+# End
